432fb326edbc087f34c153aa6bccf1b16b25261e
Wendell Valentim <wendellldasiilva8@gmail.com>
2026-04-27 18:05:19 -0300

    feat: ms-pedido created

---
+++ mspedido/src/main/java/com/io/github/wendellvalentim/mspedido/MspedidoApplication.java
@@ -0,0 +1,13 @@
+package com.io.github.wendellvalentim.mspedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MspedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MspedidoApplication.class, args);
+	}
+
+}
